a7dd00da292d3639e072a38011c88287afd8ee85
Tom Bell <tjbell@gmail.com>
2019-10-07 16:32:54 -0600

    fix: secret key

--- cms/__init__.py
+++ cms/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/{}'.format(app.root_path, 'content.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'b2de7FkqvkMyqzNFzxCkgnPKIGP6i4Rc' 
 
 db = SQLAlchemy(app)
 
