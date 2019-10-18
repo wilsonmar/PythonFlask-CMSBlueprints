0f8ed331cfd4d2ec035368796b773167b8c93acf
Tom Bell <tjbell@gmail.com>
2019-10-18 09:16:38 -0600

    module3-solution

--- cms/admin/__init__.py
+++ cms/admin/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, abort, request, redirect, url_for, flash
 
 from cms.admin.models import Content, Type, User, Setting, db
+from datetime import datetime
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin', template_folder='templates')
 
@@ -44,6 +45,32 @@ def create(type):
         return render_template('admin/content_form.html', title='Create', types=types, type_name=type)
     else:
         abort(404)
+
+@admin_bp.route('/edit/<id>', methods=('GET', 'POST'))
+def edit(id):
+    content = Content.query.get_or_404(id)
+    type = Type.query.get(content.type_id)
+    types = Type.query.all()
+
+    if request.method == 'POST':
+        content.title = request.form['title']
+        content.slug = request.form['slug']
+        content.type_id = request.form['type_id']
+        content.body = request.form['body']
+        content.updated_at = datetime.utcnow()
+        error = None
+        
+        if not request.form['title']:
+            error = 'Title is required.'
+            
+        if error is None:
+            db.session.commit()
+            return redirect(url_for('admin.content', type=type.name))
+    
+        flash(error)
+
+    types = Type.query.all()
+    return render_template('admin/content_form.html', types=types, title='Edit', item_title=content.title, slug=content.slug, type_name=type.name, type_id=content.type_id, body=content.body)
 
 @admin_bp.route('/users')
 def users():
